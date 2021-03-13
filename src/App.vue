bd9e81b7d3072fffe5067f61fb0f03b37efaa417
Lázaro Maciel <lazaromacielv@gmail.com>
2021-03-13 03:15:07 -0300

    Base do webapp

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
